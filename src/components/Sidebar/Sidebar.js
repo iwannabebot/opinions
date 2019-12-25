b408adc797498f789f48aa0dafa5d7c0ee5fd41c
Nilay Vishwakarma <nilay.vishwakarma@nagarro.com>
2019-12-25 17:47:03 +0530

    add buy me coffee link in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,11 +13,12 @@ type Props = {
 
 const Sidebar = ({ isIndex }: Props) => {
   const { author, copyright, menu } = useSiteMetadata();
-
+  const bod = '<a href="https://www.buymeacoffee.com/mUf3sLx" target="_blank"> <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" /></a>';
   return (
     <div className={styles['sidebar']}>
       <div className={styles['sidebar__inner']}>
         <Author author={author} isIndex={isIndex} />
+        <div  dangerouslySetInnerHTML={{__html: bod}} />
         <Menu menu={menu} />
         <Contacts contacts={author.contacts} />
         <Copyright copyright={copyright} />
